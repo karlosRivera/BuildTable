33a1e5a22ada52fcdff423b2a4683e7caf0367a8
tridip-bba <tridip@bba-reman.com>
2016-01-11 21:00:09 +0530

    Add one more view for working with webgrid

--- BuildTable/Controllers/HomeController.cs
+++ BuildTable/Controllers/HomeController.cs
@@ -161,6 +161,157 @@ namespace BuildTable.Controllers
             return View(oVm);
         }
 
- 
+        public ActionResult TestGrid()
+        {
+
+            MainViewModel oVm = new MainViewModel()
+            {
+                Students = new List<Student>() {
+                    new Student
+                    {
+                        ID=1,
+                        Name="JoyDev",
+                        StateID=1,
+                        CityID=1,
+                        States=new List<States>()
+                        {
+                            new States
+                            {
+                                ID=1,
+                                Name="WestBengal",
+                            },
+                            new States
+                            {
+                                ID=2,
+                                Name="Bihar",
+                            },
+                            new States
+                            {
+                                ID=3,
+                                Name="Orrisa",
+                            }
+
+                        },
+                        Cities=new List<Cities>()
+                        {
+                            new Cities
+                            {
+                                ID=1,
+                                Name="Alipur"
+                            },
+                            new Cities
+                            {
+                                ID=2,
+                                Name="Asansol"
+                            },
+                            new Cities
+                            {
+                                ID=3,
+                                Name="Andul"
+                            }
+
+                        }
+                    },
+
+//***********
+                    new Student
+                    {
+                        ID=1,
+                        Name="Mukti",
+                        StateID=2,
+                        CityID=1,
+                        States=new List<States>()
+                        {
+                            new States
+                            {
+                                ID=1,
+                                Name="WestBengal",
+                            },
+                            new States
+                            {
+                                ID=2,
+                                Name="Bihar",
+                            },
+                            new States
+                            {
+                                ID=3,
+                                Name="Orrisa",
+                            }
+
+                        },
+                        Cities=new List<Cities>()
+                        {
+                            new Cities
+                            {
+                                ID=1,
+                                Name="Janpur"
+                            },
+                            new Cities
+                            {
+                                ID=2,
+                                Name="Madhubani"
+                            },
+                            new Cities
+                            {
+                                ID=3,
+                                Name="Kanti"
+                            }
+
+                        }
+                    },
+//***********
+                    new Student
+                    {
+                        ID=1,
+                        Name="Somnath",
+                        StateID=3,
+                        CityID=2,
+                        States=new List<States>()
+                        {
+                            new States
+                            {
+                                ID=1,
+                                Name="WestBengal",
+                            },
+                            new States
+                            {
+                                ID=2,
+                                Name="Bihar",
+                            },
+                            new States
+                            {
+                                ID=3,
+                                Name="Orrisa",
+                            }
+
+                        },
+                        Cities=new List<Cities>()
+                        {
+                            new Cities
+                            {
+                                ID=1,
+                                Name="Chandapur"
+                            },
+                            new Cities
+                            {
+                                ID=2,
+                                Name="Dhankauda"
+                            },
+                            new Cities
+                            {
+                                ID=3,
+                                Name="Konarak"
+                            }
+
+                        }
+                    }
+
+
+                }
+
+            };
+
+            return View(oVm);
+        }
     }
 }
--- BuildTable/Models/MyModel.cs
+++ BuildTable/Models/MyModel.cs
@@ -8,19 +8,18 @@ namespace BuildTable.Models
     public class MainViewModel
     {
         public List<Student> Students { get; set; }
-        public int SelectedState = 0;
-        public int SelectedCity = 0;
+        public int SelectedState { get; set; }
+        public int SelectedCity { get; set; }
     }
 
     public class Student
     {
-        public int ID = 0;
-        public string Name = "";
-        public int StateID = 0;
-        public int CityID = 0;
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int StateID { get; set; }
+        public int CityID { get; set; }
         public List<States> States { get; set; }
         public List<Cities> Cities { get; set; }
-
     }
 
     public class States
