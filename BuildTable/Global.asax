c70e898beab2b830c0fc445b8a50b6e37c3a8673
tridip-bba <tridip@bba-reman.com>
2016-01-11 19:07:39 +0530

    new code repo

---
+++ BuildTable/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuildTable.MvcApplication" Language="C#" %>
---
+++ BuildTable/Models/MyModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildTable.Models
+{
+    public class MainViewModel
+    {
+        public List<Student> Students { get; set; }
+        public int SelectedState = 0;
+        public int SelectedCity = 0;
+    }
+
+    public class Student
+    {
+        public int ID = 0;
+        public string Name = "";
+        public int StateID = 0;
+        public int CityID = 0;
+        public List<States> States { get; set; }
+        public List<Cities> Cities { get; set; }
+
+    }
+
+    public class States
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Cities
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
